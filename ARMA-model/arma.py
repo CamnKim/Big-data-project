5002f8fab37d377b2c11ee7ba3ef0bfbb970fd54
Cameron Kim <ckim18@kent.edu>
2022-04-15 18:23:43 -0400

    arima model complete

--- ARMA-model/arma.py
+++ ARMA-model/arma.py
@@ -1,17 +1,58 @@
-import pandas
-import matplotlib.pyplot as mpl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-def parser(x):
-    return pandas.to_datetime(x, unit='s')
+import warnings
+warnings.filterwarnings("ignore")
 
-# get data setup and plot
-df = pandas.read_csv('data.csv', parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-#df['timestamp'] = pandas.to_datetime(df['timestamp'], unit='s') # convert ms to datetime
-df.info()
+# Read data in from csv and organize properly
+data = pd.read_csv('15-22_eth_prices.csv')
+data.columns = ['Date', 'Open']
+data['Date'] = pd.to_datetime(data['Date'], format="%m/%d/%Y")
+data = data.set_index('Date')
+data = data.iloc[::-1]                
 
-# plot data
-df.plot(kind = 'line', x = 'timestamp', y = 'open')
-#pandas.plotting.autocorrelation_plot(df['open'])
+# split into train and test | 0.929 = 2500
+train_size = (int) (len(data) * 0.9)
 
+train = list(data[0:train_size]['Open'])
+test = list(data[train_size:]['Open'])
 
-mpl.show()
+# rolling ARIMA
+model_pred = []
+num_test = len(test)
+
+for i in range(num_test):
+  model = ARIMA(train, order = (5,1,5))
+  model_fit = model.fit()
+  output = model_fit.forecast()
+  yhat = np.float64(output[0]).item()
+  model_pred.append(yhat)
+  test_val = test[i]
+
+  train.append(test_val)
+
+  print(str(i) + ': pred: ' + str(yhat) + ', actual: ' + str(test_val))
+
+print(model_fit.summary())
+
+# calculate error
+mape = np.mean(np.abs(np.array(model_pred) - np.array(test)) / np.abs(test))
+print('MAPE: ' + str(mape))
+
+# graph the data
+plt.figure(figsize=(15,9))
+plt.grid(True)
+
+date_range = data[train_size:].index
+
+plt.xlabel('Date')
+plt.ylabel('Open Prices')
+plt.title('Ethereum Price Prediction (ARIMA)')
+plt.plot(date_range, model_pred, 'red', marker='o', linestyle='dashed', label='ETH predicted')
+plt.plot(date_range, test, 'blue', label='ETH actual')
+plt.legend()
+plt.show()
---
+++ ARMA-model/pacf.py
@@ -0,0 +1,20 @@
+from tracemalloc import start
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_pacf
+
+# Read data in from csv and organize properly
+data = pd.read_csv('15-22_eth_prices.csv')
+data.columns = ['Date', 'Open']
+data['Date'] = pd.to_datetime(data['Date'], format="%m/%d/%Y")
+data = data.set_index('Date')
+
+# get first difference to make data stationary
+first_diff = data.diff()[1:]
+# plot acf to determine order for MA model
+plot_pacf(first_diff)
+
+plt.show()
+
+# from the data we see that the first lag is signifigant
